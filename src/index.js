55723a621f760ccc3ccebc7981022a29e1e836af
“Meet008” <meetchothani008@gmail.com>
2020-07-09 22:18:06 +0530

    [Redux action,reducers,connect,provider,store now connected]

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
 import App from "./containers/App";
 import * as serviceWorker from "./serviceWorker";
 import "tachyons";
+import { searchRobots } from "./reducers";
 
+const store = createStore(searchRobots);
 ReactDOM.render(
   <React.StrictMode>
     {/* <div>
@@ -12,7 +16,9 @@ ReactDOM.render(
       <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
       <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
     </div> */}
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
